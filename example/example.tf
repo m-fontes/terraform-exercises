4cd6da6e2275f475c82dc29915f3dedce0b01526
Mauricio Fontes Alexandre <mfontes@ciandt.com>
2019-02-04 19:02:15 -0200

    Enable SSH connections

--- example/example.tf
+++ example/example.tf
@@ -11,10 +11,23 @@ resource "aws_key_pair" "deployer" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcjywF//zx7SDWwE6X0Pnw95q5On8vS8g5FKm7DQKBmHBykGxB5Z2pElHXR46LKjHfvMzAB15v7ECsPZs6eR+6VWw/R6MKCav4xDlfpZd2F8A67kRsCSCE6qfQoDjZohc2P2nZHgeM5nMBihnQZ5D/mLzv8Dn9pl0rae9cVQpolZ0lIRQZRCHZcbs9x4QcydJ0MI1+cRhWQ9GjlFAbXck7Rg9JcV8YUUGV3lCOwGMAedQFXmE1eo3zG0U9MwJ6Fo9cYrdyEELH7GarqL8o0aqsqXILYrNYUmeudwzyeW3OxlgRYcwd3tw6OIws898KSAV4QqJsO3jzxZAk8yHliEQH mfontes@MACCIT014402.local"
 }
 
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   # Amazon Machine Image below is from Ubuntu Server 18.04 from South America East
   ami           = "ami-03c6239555bb12112"
   instance_type = "t2.micro"
   key_name = "mfontes-pub"
+  security_groups = ["allow_all"]
 }
 
