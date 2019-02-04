a863946d9ad6b56c060f7e3b66fb1513822173fa
Mauricio Fontes Alexandre <mfontes@ciandt.com>
2019-02-04 21:13:44 -0200

    Modify security group for all-access

--- example/example.tf
+++ example/example.tf
@@ -13,13 +13,20 @@ resource "aws_key_pair" "deployer" {
 
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
-  description = "Allow all inbound traffic"
+  description = "Allow all traffic"
 
   ingress {
     from_port   = 0
-    to_port     = 22
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 }
 
